3c239c393ac6deeeb1cf5f232787bbe5a1f07d78
eftimiedorina <dorina.elenaaa@gmail.com>
2024-01-03 15:13:12 +0200

    first commit

---
+++ Palindrom/Palindrom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserati un cuvant sau o propozitie:");
+            
+            string input = Console.ReadLine();
+            
+
+            int l=0,r = input.Length-1;
+            bool isPalindrom = false;
+            
+        
+             while( l <= r ) 
+            {
+                if (!(input[l] >= 'a' && input[l] <= 'z'))
+                    l++;
+
+                if (!(input[r] >= 'a' && input[r] <= 'z'))
+                    r--;
+                if (input[l] == input[r])
+                {
+                    isPalindrom = true;
+                    l++;
+                    r--;
+                }
+                else
+                    break;
+                    
+                
+            }
+           
+            Console.WriteLine(isPalindrom);
+
+
+        }
+    }
+}
---
+++ ROT3Cipher/ROT3Cipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ROT3Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i<sb.Length; i++) 
+            {
+                sb[i] = sb[i] 
+            }
+            
+        }
+    }
+}
