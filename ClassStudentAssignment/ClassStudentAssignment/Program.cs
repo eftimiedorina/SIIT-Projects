5039f14c367e8684c08481bd0c632998d99e775f
eftimiedorina <dorina.elenaaa@gmail.com>
2024-02-09 17:20:28 +0200

    final solution

---
+++ ClassStudentAssignment/ClassStudentAssignment/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ClassStudentAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[4];
+            byte ct = 0;
+
+
+            while (ct < 4)
+            {
+                Console.WriteLine("Enter age:");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter name:");
+                string name = Console.ReadLine();
+
+                try
+                {
+                    students[ct] = new Student(name, age);
+                    ct++;
+                }
+                catch(ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+
+            Console.WriteLine("All the students are:");
+            foreach (Student student in students) 
+            {
+                Console.WriteLine(student.Info);
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Insert a mark for {students[i].Name}:");
+                students[i].Mark = int.Parse(Console.ReadLine());
+
+
+            }
+
+            double average = 0;
+            byte cnt = 0;
+            for(int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Mark != null )
+                {
+                    average = average + students[i].Mark.Value;
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine($"The average mark is {average / cnt}");
+        }
+    }
+}
---
+++ ClassStudentAssignment/ClassStudentAssignment/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudentAssignment
+{
+    class Student
+    {
+        private string _name;
+        private int _age;
+
+        public const int MIN_AGE = 18;
+        public const int MAX_AGE = 99;
+
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set 
+            {
+                if(value < MIN_AGE)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Age must be at least {MIN_AGE}");
+                else if(value > MAX_AGE)
+                    throw new ArgumentOutOfRangeException(nameof(value), $"Age cannot exceed {MAX_AGE}");
+                else
+                {
+                    _age = value;
+                }
+
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int? Mark
+        {
+            get; set;
+        }
+
+        public string Info
+        {
+            get { return _name + " " + _age; }
+        }
+    }
+}
